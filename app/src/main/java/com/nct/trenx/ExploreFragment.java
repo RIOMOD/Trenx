40b9503b8914a1ec618c7a729b2dabb15af4b6cd
nguyencongtru <thu113112111@gmail.com>
2026-05-25 23:41:29 +0700

    "Cập nhật giao diện màn hình chuẩn bị tập"

--- app/src/main/java/com/nct/trenx/ExploreFragment.java
+++ app/src/main/java/com/nct/trenx/ExploreFragment.java
@@ -1,6 +1,20 @@
 package com.nct.trenx;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 public class ExploreFragment extends Fragment {
+
+    public ExploreFragment() {
+        // Constructor rỗng
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // CHÚ Ý DÒNG NÀY: Phải gọi đúng chữ R.layout.fragment_explore
+        return inflater.inflate(R.layout.fragment_explore, container, false);
+    }
 }
--- app/src/main/java/com/nct/trenx/MyProgressFragment.java
+++ app/src/main/java/com/nct/trenx/MyProgressFragment.java
@@ -1,6 +1,21 @@
 package com.nct.trenx;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 public class MyProgressFragment extends Fragment {
+
+    public MyProgressFragment() {
+        // Constructor rỗng
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // CHÚ Ý DÒNG NÀY: Phải gọi đúng chữ R.layout.fragment_explore
+// Sửa dòng 18 thành thế này:
+        return inflater.inflate(R.layout.myprogress_community, container, false);
+    }
 }
